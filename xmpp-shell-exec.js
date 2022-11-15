ef73dccf28b43c2baa7f7955eebaf0f6c11888b4
pocketfood <crazykozy@protonmail.ch>
2022-11-15 18:54:36 -0500

    Update xmpp-shell-exec.js

--- xmpp-shell-exec.js
+++ xmpp-shell-exec.js
@@ -11,106 +11,115 @@ const xmpp = client({
   username: "",
   password: "",
 });
-
-xmpp.on("online", async (username) => {
+xmpp.on("online", async (jid) => {
   
-  // Timestamps when bot goes online
-  // This is good to see and log when bot goes online
-  const date = new Date();
-  console.log('\x1b[1m\x1b[36m','Bot Started on ',date,'\x1b[0m')
-
-  // sends the date to itself
-  // put back after done to: username, so it send to itself
-
-  const message = xml(
-    "message",
-    { type: "chat", to: "" },
-    xml("body", {}, 'Bot started on ', date),
-  );
-  await xmpp.send(message);
-});
-
-console.log('\x1b[33m%s\x1b[0m', '--- XMPP Connection ---')
-xmpp.start().catch(console.error), () => {
+    // Timestamps when bot goes online
+    // This is good to see and log when bot goes online
+    const date = new Date();
+    console.log('\x1b[1m\x1b[36m','Bot Started on ',date,'\x1b[0m')
   
-};
-
-// This displays all the body messages
-xmpp.on("stanza", (stanza) => {
-  bodymessage = stanza.getChildText('body')
-  console.log('\n','\x1b[1m\x1b[36m','Message:',bodymessage,'\x1b[0m','\n')
-});  
-
-
-xmpp.on("offline", () => {
-    console.log("bot offline");
+    // sends the date to itself
+    // put back after done to: username, so it send to itself
+  
+    const message = xml(
+      "message",
+      { type: "chat", to: jid },
+      xml("body", {}, 'Bot started on ', date),
+    );
+    await xmpp.send(message);
   });
-
-  xmpp.on("stanza", (stanza) => {
-    console.log(stanza.toString());
-    console.log('\x1b[33m%s\x1b[0m', '--- XML Stream ---');
+  
+  console.log('\x1b[33m%s\x1b[0m', '--- XMPP Connection ---')
+  xmpp.start().catch(console.error), () => {
     
-  });
-
-  // Commands start here
-
+  };
+  
+  // This displays all the body messages
   xmpp.on("stanza", (stanza) => {
     
-  // reads message and outputs into terminal
-  console.log('\x1b[33m%s\x1b[0m', '--- XML Stream From User ---');    
-  // The const that makes this all happen
-  // The most important CONST
-  const text  = stanza.getChildText('body');
-  if(text) {
-    const split = text.split(/(?<=^\S+)\s/)
-    const possiblyBash = split[0]
-    const command = split[1]
-    // full string
-    console.log(split, possiblyBash, command)
-    console.log(possiblyBash === 'bash')
-
-    if(possiblyBash === 'bash')  {
-      exec(command, async (error, stdout, stderr) => {
-        if (error) {         
-            // Sends the command output to user if command not found
-            console.log(`error: ${error.message}`);
-            const { to, from } = stanza.attrs;
-            stanza.attrs.from = to;
-            stanza.attrs.to = from;
-            const message = xml(
-              "message",
-              { type: "chat", to: from },
-              xml("body", {}, `${error}`),
-              ); 
-              xmpp.send(message);  
-            return;
+    bodymessage = stanza.getChildText('body')
+        if (bodymessage)
+        {
+        console.log('\n','\x1b[1m\x1b[36m','Message:',bodymessage,'\x1b[0m','\n')
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
+  });  
+  
+  
+  xmpp.on("offline", () => {
+      console.log("bot offline");
+    });
+  
+    // Displays Presence
+    xmpp.on("stanza", (presence) => {
+        if (presence)
+        {
+      console.log(presence.toString());
+      console.log('\x1b[33m%s\x1b[0m', '--- PRESENCE ---');
         }
-        console.log(`${stdout}`);
-
-        // Sends the command output to user
-        const { to, from } = stanza.attrs;
-        stanza.attrs.from = to;
-        stanza.attrs.to = from;
-
-        const message = xml(
-        "message",
-        { type: "chat", to: from },
-        xml("body", {}, `${stdout}`) ,
-        ); 
-        xmpp.send(message);
-      });       
+    });
+  
+    // Commands start here
+  
+    xmpp.on("stanza", (stanza) => {
+      
+    // reads message and outputs into terminal
+    console.log('\x1b[33m%s\x1b[0m', '--- XML Stream From User ---');    
+    
+    // The const that makes this all happen
+    // The most important CONST
+    
+    const text  = stanza.getChildText('body');
+    
+    if(text) {
+      const split = text.split(/(?<=^\S+)\s/)
+      const possiblyBash = split[0]
+      const command = split[1]
+      // full string
+      console.log(split, possiblyBash, command)
+      console.log(possiblyBash === 'bash')
+  
+      if(possiblyBash === 'bash')  {
+        exec(command, async (error, stdout, stderr) => {
+          if (error) {         
+              // Sends the command output to user if command not found
+              console.log(`error: ${error.message}`);
+              const { to, from } = stanza.attrs;
+              stanza.attrs.from = to;
+              stanza.attrs.to = from;
+              const message = xml(
+                "message",
+                { type: "chat", to: from },
+                xml("body", {}, `${error}`),
+                ); 
+                xmpp.send(message);  
+              return;
+          }
+          if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              return;
+          }
+          console.log(`${stdout}`);
+  
+          // Sends the command output to user
+          const { to, from } = stanza.attrs;
+          stanza.attrs.from = to;
+          stanza.attrs.to = from;
+  
+          const message = xml(
+          "message",
+          { type: "chat", to: from },
+          xml("body", {}, `${stdout}`) ,
+          ); 
+          xmpp.send(message);
+        });    
+        // else just console log the received message
+        console.log('\x1b[1m\x1b[32m','--- Heres a command ---','\x1b[0m');    
+      } 
     }
-    // else just console log the received message
-    console.log('\x1b[1m\x1b[32m','--- Heres a command ---','\x1b[0m');  
-  }
-}); 
-
-xmpp.on("online", (address) => {
-  console.log('\x1b[1m\x1b[36m','Online as',address.toString(),'\x1b[0m','\n');
-  xmpp.send(xml("presence")).catch(console.error);
-
-});
+  }); 
+  
+  xmpp.on("online", (address) => {
+    console.log('\x1b[1m\x1b[36m','Online as',address.toString(),'\x1b[0m','\n');
+    xmpp.send(xml("presence")).catch(console.error);
+  
+  });
