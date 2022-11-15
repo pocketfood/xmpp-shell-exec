2290304946d2d2b5b93a80a31119f47a28ea5590
Vincent Arlou <54914693+vinclou@users.noreply.github.com>
2022-11-14 20:38:14 -0600

    Update xmpp-shell-exec.js

--- xmpp-shell-exec.js
+++ xmpp-shell-exec.js
@@ -61,50 +61,52 @@ xmpp.on("offline", () => {
   // The const that makes this all happen
   // The most important CONST
   const text  = stanza.getChildText('body');
-  const split = text.split(/(?<=^\S+)\s/)
-  const possiblyBash = split[0]
-  const command = split[1]
-  // full string
-  console.log(split, possiblyBash, command)
-  console.log(possiblyBash === 'bash')
+  if(text) {
+    const split = text.split(/(?<=^\S+)\s/)
+    const possiblyBash = split[0]
+    const command = split[1]
+    // full string
+    console.log(split, possiblyBash, command)
+    console.log(possiblyBash === 'bash')
 
-  if(possiblyBash === 'bash')  {
-    exec(command, async (error, stdout, stderr) => {
-      if (error) {         
-          // Sends the command output to user if command not found
-          console.log(`error: ${error.message}`);
-          const { to, from } = stanza.attrs;
-          stanza.attrs.from = to;
-          stanza.attrs.to = from;
-          const message = xml(
-            "message",
-            { type: "chat", to: from },
-            xml("body", {}, `${error}`),
-            ); 
-            xmpp.send(message);  
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      console.log(`${stdout}`);
-  
-      // Sends the command output to user
-      const { to, from } = stanza.attrs;
-      stanza.attrs.from = to;
-      stanza.attrs.to = from;
-  
-      const message = xml(
-      "message",
-      { type: "chat", to: from },
-      xml("body", {}, `${stdout}`) ,
-      ); 
-      xmpp.send(message);
-    });       
+    if(possiblyBash === 'bash')  {
+      exec(command, async (error, stdout, stderr) => {
+        if (error) {         
+            // Sends the command output to user if command not found
+            console.log(`error: ${error.message}`);
+            const { to, from } = stanza.attrs;
+            stanza.attrs.from = to;
+            stanza.attrs.to = from;
+            const message = xml(
+              "message",
+              { type: "chat", to: from },
+              xml("body", {}, `${error}`),
+              ); 
+              xmpp.send(message);  
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+
+        // Sends the command output to user
+        const { to, from } = stanza.attrs;
+        stanza.attrs.from = to;
+        stanza.attrs.to = from;
+
+        const message = xml(
+        "message",
+        { type: "chat", to: from },
+        xml("body", {}, `${stdout}`) ,
+        ); 
+        xmpp.send(message);
+      });       
+    }
+    // else just console log the received message
+    console.log('\x1b[1m\x1b[32m','--- Heres a command ---','\x1b[0m');  
   }
-  // else just console log the received message
-  console.log('\x1b[1m\x1b[32m','--- Heres a command ---','\x1b[0m');  
 }); 
 
 xmpp.on("online", (address) => {
